644abb6b51722337486667daaed77ec4f26bc1b7
Jimmy Barnett <51797234+trejimmy5562@users.noreply.github.com>
2023-04-26 19:37:38 -0500

    small edit

--- autobirthdays.py
+++ autobirthdays.py
@@ -4,8 +4,8 @@ import time
 from twilio.rest import Client 
 
 # Twilio account SID and auth token
-account_sid = 'AC8fe443806b782ae0a6881a80f23a78c5'
-auth_token = '472f1c10001fdc5d72ffc341b9da7941'
+account_sid = 'xxx'
+auth_token = 'xxx'
 client = Client(account_sid, auth_token)
 
 # Open the CSV file and parse the names and dates
@@ -27,8 +27,8 @@ while True:
     for i in range(len(dates)):
         if current_month == int(dates[i][:2]) and current_day == int(dates[i][3:]):
             message = client.messages.create(
-                to='+14694694593',
-                from_='+18776294436',
+                to='+xxx',
+                from_='+xxx',
                 body=f"Today is {names[i]}'s Birthday! Don't forget to TEXT THEM!"
             )
             print('Message sent at', datetime.datetime.now())
